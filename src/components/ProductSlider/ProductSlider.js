13b52ae1a4780105cd6028924002a2071cc6b5c0
gopalan10 <gopal@feelvaleo.com>
2023-05-16 14:56:32 +0530

    fixed desktop view

--- src/components/ProductSlider/ProductSlider.js
+++ src/components/ProductSlider/ProductSlider.js
@@ -1,4 +1,5 @@
 'use client'
+import React,{useState,useEffect} from 'react';
 import ProductCard from '@/components/ProductCard/ProductCard';
 import styles from './product-slider.module.scss';
 import Glider from 'react-glider';
@@ -6,6 +7,13 @@ import "glider-js/glider.min.css";
 
 const ProductSlider = ({backgroundColor,topColor,design}) => {
  
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleResize = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [width]);
+
     return (
 
 
@@ -20,13 +28,13 @@ const ProductSlider = ({backgroundColor,topColor,design}) => {
             <div className={styles.sliderContainer}>
             <Glider
               hasArrows
-              slidesToShow={4}
+              slidesToShow={4.5}
               slidesToScroll={4}
               hasDots
               draggable
               gap={20}
               exactWidth={true}
-              itemWidth={204}
+              itemWidth={(width>990)?204:138}
               iconLeft={
                 <img style={{width:48,height:48}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/left_arrow.png' alt='left-icon'/>
               }
---
+++ src/hooks/resizeHook.js
@@ -0,0 +1,12 @@
+const useWidth = () => {
+    if (process.browser) {
+      const [width, setWidth] = useState(window.innerWidth);
+      const handleResize = () => setWidth(window.innerWidth);
+      useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, [width]);
+      return width;
+    }
+    return 0;
+  };
