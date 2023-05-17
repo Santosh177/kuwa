fe11ba433ae6e2c1e5d8f1369c58c06174adc5f1
gopalan10 <gopal@feelvaleo.com>
2023-05-17 15:05:50 +0530

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default async function Home({data}) {
 
   return (
     <>
-        <Header />
+        {/* <Header /> */}
         {/* <AssuredInfo />
            <ProductSlider />
         <ProductSlider />
