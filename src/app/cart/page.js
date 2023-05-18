5af97769105cfdeaecf2870db5c3db9c1ae0a11f
gopalan10 <gopal@feelvaleo.com>
2023-05-18 22:44:03 +0530

    fixed

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,6 +1,7 @@
 
 import CartItemCard from "@/components/CartItemCard/CartItemCard"
 import PriceDetails from "@/components/PriceDetails/PriceDetails";
+import styles from './page.module.scss';
 
 export default function AllProduct() {
 
@@ -9,13 +10,22 @@ export default function AllProduct() {
     console.log("ht")
   
       return (
-        <>
-          <div> Cart </div>
-          <CartItemCard />
-          <CartItemCard />
-          <div>PriceDetails</div>
-          <PriceDetails />
-        </>
+        <div className={styles.cartPage}>
+          <div className={styles.cartItemsContainer}>
+            <div className={styles.headerTxt}> Cart Items </div>
+            <CartItemCard />
+            <CartItemCard />
+          </div>
+          <div className={styles.priceDetailsContainer}>
+            <div className={styles.headerTxt}>PriceDetails</div>
+            <div className={styles.priceInfo}>
+              <PriceDetails />
+            </div>
+            
+          </div>
+         
+          
+        </div>
       )
     }
     
---
+++ src/components/CompanyInfo/CompanyInfo.js
@@ -0,0 +1,21 @@
+
+import styles from './company.module.scss';
+
+
+
+const CompanyInfo = ({number,image}) => {
+
+    return(
+        <div>
+            CompanyInfo
+        </div>
+    )
+
+
+
+}
+
+
+export default CompanyInfo;
+
+  
