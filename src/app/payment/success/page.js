e2d754ba79a9c9e7e8e5163363d5d6f88b9b802d
gopalan10 <gopal@feelvaleo.com>
2023-05-19 22:53:09 +0530

    added payment success page

--- src/app/payment/success/page.js
+++ src/app/payment/success/page.js
@@ -7,10 +7,11 @@ export default function PaymentSuccess() {
 
   
       return (
-        <div>
+        <div className={styles.paymentSuceesContainer}>
           <img src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/success.png' alt=''/>
-          <div>Order placed</div>
-          <div>Thanks for your purchase! Confirmation email with details coming soon. Contact us if you have any questions.</div>
+          <div className={styles.txt}>Order placed</div>
+          <div className={styles.subTxt}>Thanks for your purchase! Confirmation email with details coming soon. Contact us if you have any questions.</div>
+          <div className={styles.btn}>Continue Shopping</div>
         </div>
       )
     }
