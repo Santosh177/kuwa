fe5cd678dc14fc553ed8c3c4503bc5fe2902cfdb
gopalan10 <gopal@feelvaleo.com>
2023-05-20 16:49:57 +0530

    mobile header added

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,18 +1,15 @@
 
 import styles from './page-header.module.scss';
 
-// https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background+(1).png
-// https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background_desktop.png
 const PageHeader = ({}) => {
 
     return(
         <div className={styles.pageHeader}>
             <img className={styles.headerBackgroundImg} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background_desktop.png' />
+            <img className={styles.mobHeaderBackgroundImg} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background+(1).png' />
             <div className={styles.headerContent}>
-                {/* <div className={styles.headerInfo}> */}
-                    <img className={styles.backArrow} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/back_arrow+%281%29.png' alt='back-arrow'/>
-                        <div className={styles.headerTxt}>Payment</div>
-                {/* </div> */}
+                <img className={styles.backArrow} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/back_arrow+%281%29.png' alt='back-arrow'/>
+                <div className={styles.headerTxt}>Payment</div>
                 <img className={styles.kuwaLogo} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/kuwa_logo.png' alt='kuwa-logo'/>
             </div>
            
