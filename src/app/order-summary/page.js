2f2ffbc82324f8524b11870d305ef8984e54e01d
gopalan10 <gopal@feelvaleo.com>
2023-05-21 00:12:44 +0530

    fixed

--- src/app/order-summary/page.js
+++ src/app/order-summary/page.js
@@ -1,7 +1,9 @@
-
+import PageHeader from "@/components/PageHeader/PageHeader";
+import PageStepTracker from "@/components/PageStepTracker/PageStepTracker";
 import DeliveryAddress from "./DeliveryAddress/DeliveryAddress";
 import CartItemCard from "@/components/CartItemCard/CartItemCard";
 import PriceDetails from "@/components/PriceDetails/PriceDetails";
+import PatmentFooterBtn from "@/components/PaymentFooterBtn/PaymentFooterBtn";
 import styles from './pages.module.scss';
 
 export default function AllProduct() {
@@ -11,18 +13,24 @@ export default function AllProduct() {
     console.log("ht")
   
       return (
-        <div className={styles.orderSummary}>
-          <div className={styles.addressAndProductDetails}> 
-            <div className={styles.headerTxt}>Address & product details</div>
-            <DeliveryAddress />
-            <CartItemCard />
-            <CartItemCard />
-          </div>
-          <div className={styles.priceDetails}>
-            <div className={styles.headerTxt}>Price Details</div>
-            <PriceDetails />
-          </div>
-        </div>
+        <>
+          <PageHeader headerName="Order Summary"/>
+          <PageStepTracker stepCount={2} />
+         
+          {/* <div className={styles.orderSummary}>
+            <div className={styles.addressAndProductDetails}> 
+              <div className={styles.headerTxt}>Address & product details</div>
+              <DeliveryAddress />
+              <CartItemCard />
+              <CartItemCard />
+            </div>
+            <div className={styles.priceDetails}>
+              <div className={styles.headerTxt}>Price Details</div>
+              <PriceDetails />
+            </div>
+          </div> */}
+          <PatmentFooterBtn btnName="Proceed to next"  totalPrice="AED  350"/>
+        </>
       )
     }
     
