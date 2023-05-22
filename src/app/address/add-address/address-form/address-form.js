5f3d15e5acb0ad8b385ee60f6e214770a6cbbbb8
gopalan10 <gopal@feelvaleo.com>
2023-05-23 03:25:25 +0530

    add address

--- src/app/address/add-address/address-form/address-form.js
+++ src/app/address/add-address/address-form/address-form.js
@@ -4,39 +4,41 @@ import Input from "@/components/Input/Input";
 import PhoneNumberInput from "@/components/PhoneNumberInput/PhoneNumberInput";
 import CreateAccountBox from "../components/CreateAccountBox/CreateAccountBox";
 import styles from './address-form.module.scss';
+import { useState } from "react";
 
-const PersonalInfoFrom = () => {
+const PersonalInfoFrom = ({onChange={},values={}}) => {
+
 
     return (
         <div className={styles.personalInfoForm}>
             <div className={styles.headerTxt}>Personal Info</div>
             <CreateAccountBox />
             <div className={styles.userNameContainer}>
-                <Input type="text" fieldName="firstName" placeHolder="First name *" style={{width:'49%'}}  />
-                <Input type="text" fieldName="lastName" placeHolder="Last name *" style={{width:'49%'}}  />
+                <Input type="text" fieldName="firstName" placeHolder="First name *" style={{width:'49%'}}  value={values['firstName']} onInputChange={onChange} />
+                <Input type="text" fieldName="lastName" placeHolder="Last name *" style={{width:'49%'}} value={values['lastName']} onInputChange={onChange} />
             </div>
             <PhoneNumberInput />
-            <Input type="email" fieldName="email" placeHolder="Email ID (ex. abc@gmail.com)"  />
+            <Input type="email" fieldName="email" placeHolder="Email ID (ex. abc@gmail.com)" value={values['email']} onInputChange={onChange}   />
         </div>
     )
 }
 
-const AddressInfoForm = () => {
+const AddressInfoForm = ({onChange={},values={}}) => {
     return (
         <div className={styles.addressInfoForm}>
          <div className={styles.headerTxt}>Address</div>
             <div className={styles.areaInputText}>
                 <div className={styles.inputContainer}>
-                    <input type="text" id='area' name='area' autocomplete="off" value={''} onChange={(e)=>onInputChange(e,fieldName)} />
+                    <input type="text" id='area' name='area' autocomplete="off" value={values['area']} onChange={(e)=>onChange(e,"area")} />
                     <label className={styles.placeholderText}>
                         <div className={styles.text}>Area name, Colony *</div>
                     </label>
                 </div>
             </div>
-            <Input type="email" fieldName="email" placeHolder="Appartment name, Floor, Room no, City*"  />
+            <Input type="text" fieldName="apartment" placeHolder="Appartment name, Floor, Room no, City*" value={values['apartment']} onInputChange={onChange}  />
             <div className={styles.countryContainer}>
-                <Input type="text" fieldName="country" placeHolder="Country *"  style={{width:'49%'}} />
-                <Input type="text" fieldName="stateProvince" placeHolder="State Province*" style={{width:'49%'}}  />
+                <Input type="text" fieldName="country" placeHolder="Country *"  style={{width:'49%'}} value={values['country']} onInputChange={onChange}  />
+                <Input type="text" fieldName="stateProvince" placeHolder="State Province*" style={{width:'49%'}} value={values['stateProvince']} onInputChange={onChange}   />
             </div>
         </div>
     )
@@ -45,15 +47,25 @@ const AddressInfoForm = () => {
 
 export default function AddressForm() {
 
+      const [ values , setValues ] = useState({});
+
+      const onChange = (e,fieldName) => {
+        const value = e.target.value;
+        console.log("fieldName",fieldName)
+        setValues(currentValues =>({...currentValues,[fieldName]:value}))
+        
+      }
 
 
+
+      console.log("values",values)
   
       return (
         <>
           <div className={styles.addressForm}> 
         
-            <PersonalInfoFrom />
-            <AddressInfoForm />
+            <PersonalInfoFrom onChange={onChange} values={values} />
+            <AddressInfoForm onChange={onChange} values={values}/>
           </div>
         </>
       )
