08b850d4aedff8da40d2f364f08fb5b96f2894d2
gopalan10 <gopal@feelvaleo.com>
2023-05-22 14:42:39 +0530

    order summary page

--- src/app/order-summary/page.js
+++ src/app/order-summary/page.js
@@ -1,9 +1,6 @@
 import PageHeader from "@/components/PageHeader/PageHeader";
 import PageStepTracker from "@/components/PageStepTracker/PageStepTracker";
-import DeliveryAddress from "./DeliveryAddress/DeliveryAddress";
-import CartItemCard from "@/components/CartItemCard/CartItemCard";
-import PriceDetails from "@/components/PriceDetails/PriceDetails";
-import PatmentFooterBtn from "@/components/PaymentFooterBtn/PaymentFooterBtn";
+import OrderSummaryPage from "./OrderSummaryPage/OrderSummaryPage";
 import styles from './pages.module.scss';
 
 export default function AllProduct() {
@@ -16,22 +13,7 @@ export default function AllProduct() {
         <>
           <PageHeader headerName="Order Summary"/>
           <PageStepTracker stepCount={2} />
-         
-          <div className={styles.orderSummary}>
-            <div className={styles.addressAndProductDetails}> 
-              <div className={[styles.headerTxt,styles.addressTxt].join(" ")}>Address & product details</div>
-              <DeliveryAddress />
-              <CartItemCard />
-              <CartItemCard />
-            </div>
-            <div className={styles.priceDetails}>
-              <div className={styles.headerTxt}>Price Details</div>
-              <div className={styles.priceDetailsContainer}>
-                <PriceDetails />
-              </div>
-            </div>
-          </div>
-          <PatmentFooterBtn btnName="Proceed to next"  totalPrice="AED  350"/>
+          <OrderSummaryPage />
         </>
       )
     }
