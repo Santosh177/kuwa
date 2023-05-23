ac7f6c5b2514ad39876abc68ee992a0ce2cb3190
gopalan10 <gopal@feelvaleo.com>
2023-05-23 11:59:28 +0530

    fixed

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -5,15 +5,21 @@ import CartPage from './cartPage';
 import { authHeader } from "../../lib/auth-cookies";
 
 export default async function Cart() {
-  const customHeader = await authHeader();
-  const getCartItemResp  =  await fetch('https://api.kuwa.bevaleo.dev/api/v1/cart', {
-    method: 'GET',
-    headers:{
-      ...customHeader
-    },
-    next: { revalidate: 0} 
-  })
-  const getCartItems = await getCartItemResp.json();
+  let getCartItems = []
+  try {
+    const customHeader = await authHeader();
+    const getCartItemResp  =  await fetch('https://api.kuwa.bevaleo.dev/api/v1/cart', {
+      method: 'GET',
+      headers:{
+        ...customHeader
+      },
+      next: { revalidate: 0} 
+    })
+     getCartItems = await getCartItemResp.json();
+  } catch (error) {
+    getCartItems = []
+  }
+
 
   
       return (
