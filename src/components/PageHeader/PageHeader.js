3e0bcdcd7824f90389a770e5175026ce4d064e0e
gopalan10 <gopal@feelvaleo.com>
2023-05-23 18:18:23 +0530

    fixed

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,19 @@
-
+'use client'
+import { useRouter } from 'next/navigation';
 import styles from './page-header.module.scss';
 
 const PageHeader = ({headerName=""}) => {
+
+    const router = useRouter()
+
+    console.log("routerrouter",router)
 
     return(
         <div className={styles.pageHeader}>
             <img className={styles.headerBackgroundImg} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background_desktop.png' />
             <img className={styles.mobHeaderBackgroundImg} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/header_background+(1).png' />
             <div className={styles.headerContent}>
-                <img className={styles.backArrow} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/back_arrow+%281%29.png' alt='back-arrow'/>
+                <img className={styles.backArrow} onClick={()=>router.back()} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/back_arrow+%281%29.png' alt='back-arrow'/>
                 <div className={styles.headerTxt}>{headerName}</div>
                 <img className={styles.kuwaLogo} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/kuwa_logo.png' alt='kuwa-logo'/>
             </div>
