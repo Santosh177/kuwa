051eca9d4288fdeb4f30a42a68c997ba2ce171aa
gopalan10 <gopal@feelvaleo.com>
2023-05-24 04:29:41 +0530

    applied coupon

--- src/app/payment/components/CouponCode/CouponCode.js
+++ src/app/payment/components/CouponCode/CouponCode.js
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import styles from './coupon-code.module.scss';
 const Input = ({onInputChange={},type="text",fieldName="",value="",placeHolder="",isError="",errorMsg="",isDisabled="",style={}}) => {
   return(
@@ -16,16 +17,32 @@ const Input = ({onInputChange={},type="text",fieldName="",value="",placeHolder="
 }
 export default function CouponCode() {
 
+     const [couponCode , setCouponCode] = useState("")
 
 
+
+     console.log("couponCode",couponCode)
+
+     const onCouponApply = () => {
+
+     }
   
       return (
         <div className={styles.couponCodeWrapper}>
             <div className={styles.headerTxt}>Discount code or Gift card</div>
             <div className={styles.couponCodeContainer}>
-                <Input type="text" fieldName="couponCode" placeHolder="Enter coupon code" style={{width:'49%'}}  />
-                <div className={styles.applyBtn}> Apply</div>
+                <div style={{width:'100%',position:'relative'}}>
+                  <Input type="text" fieldName="couponCode" placeHolder="Enter coupon code" value={couponCode} onInputChange={(e,fieldName)=>{
+console.log("e.taregt.value",e.target.value)
+setCouponCode(e.target.value)
+                  }
+                      
+                    }  />
+                  <img onClick={()=>setCouponCode("")} className={styles.removeIcon} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/cross_icon.png' alt='close'/>
+                </div>
+                <div className={styles.applyBtn} onClick={onCouponApply}> Apply</div>
             </div>
+            {/* <div className={styles.couponMsg}>Coupon Applied ! You get AED 30 OFF</div> */}
         </div>
       )
     }
