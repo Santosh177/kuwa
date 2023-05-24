a12fca5951e87f701d1fa6a3bd3a7f523c54039a
gopalan10 <gopal@feelvaleo.com>
2023-05-24 20:08:49 +0530

    Fixed image issues

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -17,7 +17,7 @@ const ProductCard = ({cardData,addToCart={}}) => {
                 </div>
                 <div className={styles.productName}>{productName}</div>
                { <div className={styles.discountTag} style={(discount > 0)?{opacity:1}:{opacity:0}}><span>Save</span> {currency} {discount}</div>}
-                <div className={styles.price}>{currency} {finalPrice} <span className={[styles.price,styles.retailPrice].join(" ")}>{currency} {retailPrice}</span></div>
+                {(discount > 0)?<div className={styles.price}>{currency} {finalPrice}  <span className={[styles.price,styles.retailPrice].join(" ")}>{currency} {retailPrice}</span></div>:<div className={styles.price}>{currency} {finalPrice} </div>}
                 <div className={styles.btn} onClick={()=>addToCart()}>Add to cart</div>
             </div>
         </div>
--- src/components/ProductSlider/ProductSlider.js
+++ src/components/ProductSlider/ProductSlider.js
@@ -79,7 +79,8 @@ const ProductSlider = ({backgroundColor,topColor,design,data}) => {
                     retailPrice:retailPrice,
                     currency:currency,
                     discount:discount,
-                    discountType:discountType
+                    discountType:discountType,
+                    image:data.image || ""
                   }
                   return(
                     <ProductCard  cardData={cardData} addToCart={()=>onAddToCart({product:data.id,quantity:1})} />
