d65829873556919781d60dd7c6632b803c29c912
gopalan10 <gopal@feelvaleo.com>
2023-05-24 17:17:12 +0530

    fixed

--- src/app/Home/AssuredInfo/AssuredInfo.js
+++ src/app/Home/AssuredInfo/AssuredInfo.js
@@ -10,9 +10,9 @@ const AssuredInfoCard = () => {
     return(
         <div className={styles.assuredInfoCard}>
             <div className={styles.assuredCard}>
-                    {/* <div className={styles.assuredIcon}>
+                    <div className={styles.assuredIcon}>
                     <img src='https://production-website-builds.s3.ap-south-1.amazonaws.com/assets/assuredInfo.png' alt='logo'></img>
-                    </div> */}
+                    </div>
                     <div className={styles.assuredTxt}>Free Home Delivery</div>
                     <div className={styles.assuredSubTxt}>Above 100 AED</div>
             </div>
@@ -26,115 +26,15 @@ const AssuredInfoCard = () => {
 const AssuredInfo = () => {
 
     return(
+
+        <div className={styles.assuredInfoWrapper}>
+          <AssuredInfoCard/>
+          <AssuredInfoCard />
+          <AssuredInfoCard />
+
+        </div>
            
-        <Glider
-        className={styles.gliderWre}
-        slidesToShow={3}
-        slidesToScroll={3}
-        iconLeft={
-          <img style={{width:48,height:48}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/left_arrow.png' alt='left-icon'/>
-        }
-        iconRight={
-          <img style={{width:48,height:48}}  src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/right_arrow.png' alt='right-icon'/>
-        }
-        responsive={[
-          {
-            breakpoint: 320,
-            settings: {
-              slidesToShow: 1.2,
-            },
-          },
-          {
-            breakpoint: 360,
-            settings: {
-              slidesToShow: 1.4,
-            },
-          },
-          {
-            breakpoint: 380,
-            settings: {
-              slidesToShow: 1.5,
-            },
-          },
-          {
-            breakpoint: 400,
-            settings: {
-              slidesToShow: 1.6,
-            },
-          },
-          {
-            breakpoint: 440,
-            settings: {
-              slidesToShow: 1.7,
-            },
-          },
-          {
-            breakpoint: 470,
-            settings: {
-              slidesToShow: 1.8,
-            },
-          },
-          {
-            breakpoint: 499,
-            settings: {
-              slidesToShow: 1.9,
-            },
-          },
-          {
-            breakpoint: 500,
-            settings: {
-              slidesToShow: 1.9,
-            },
-          },
-          {
-            breakpoint: 530,
-            settings: {
-              slidesToShow: 2.1,
-            },
-          },
-          {
-            breakpoint: 580,
-            settings: {
-              slidesToShow: 2.3,
-            },
-          },
-          {
-            breakpoint: 620,
-            settings: {
-              slidesToShow: 2.5,
-            },
-          },
-          {
-            breakpoint: 660,
-            settings: {
-              slidesToShow: 2.6,
-            },
-          },
-          {
-            breakpoint: 710,
-            settings: {
-              slidesToShow: 2.8,
-            },
-          },
-          {
-            breakpoint: 760,
-            settings: {
-              slidesToShow: 2.9,
-            },
-          },
-        ]}
-      >
 
-        <AssuredInfoCard />
-         <AssuredInfoCard />
-        <AssuredInfoCard />
-        {/* <AssuredInfoCard /> */}
-
-       </Glider>
-
- 
-     
-   
     )
 
 
--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default async function Home({}) {
   return (
     <>
         <Header />
-        {/* <AssuredInfo /> */}
+        <AssuredInfo />
         <BrandMustTry />
         {
           data.map((data,index)=>{
