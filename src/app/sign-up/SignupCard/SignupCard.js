9b6195621f1efdda999c70e8f89a6edbd6a502bb
gopalan10 <gopal@feelvaleo.com>
2023-05-25 22:22:29 +0530

    fixed

--- src/app/sign-up/SignupCard/SignupCard.js
+++ src/app/sign-up/SignupCard/SignupCard.js
@@ -5,8 +5,32 @@ import PhoneNumberInput from '@/components/PhoneNumberInput/PhoneNumberInput';
 import styles from './sign-up-card.module.scss';
 import { useState } from 'react';
 
+const validateForm = (formData) => {
+  const errors = {};
+  if (!formData.firstName) {
+    errors.firstName = 'First name is required.';
+  }
+  if (!formData.lastName) {
+    errors.lastName = 'Last name is required.';
+  }
+  if(!formData.mobNumber){
+    errors.mobNumber = "Mobile number is required";
+  }
+  if(!formData.email){
+    errors.email = "Email is required";
+  }else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    errors.email = 'Invalid email address.';
+  }
+  if(!formData.password){
+    errors.password = "Password is required";
+  }else if(!formData.password.length > 7){
+    errors.password = "Passwords need to be a min. of 8 characters";
+  }
+  return errors;
+};
 
-const SignupForm = ({setFormData={},formData={}}) => {
+
+const SignupForm = ({setFormData={},formData={},errors={}}) => {
 
   const onInputChange = (event, labelId) =>{
     if(labelId === 'mobNumber'){
@@ -24,23 +48,24 @@ const SignupForm = ({setFormData={},formData={}}) => {
             <div className={styles.userNameContainer}>
                 <div className={styles.inputContain}>
                     <Input type="text" id="fname" name="fname" placeHolder='First name *'  value={formData.firstName || ""} onInputChange={(e)=>onInputChange(e,'firstName')}  />
+                    {errors.firstName && <span className={styles.errorMsg}>{errors.firstName}</span>}
                 </div>
                 <div className={styles.inputContain}>
                     <Input type="text" id="lname" name="fname" placeHolder='Last name *'  value={formData.lastName || ""} onInputChange={(e)=>onInputChange(e,'lastName')}  />
+                    {errors.firstName && <span className={styles.errorMsg}>{errors.lastName}</span>}
                 </div>
             </div>
             <div>
             <PhoneNumberInput type="text" fieldName="mobNumber"   value={formData.mobNumber || ""} onInputChange={onInputChange} />
-                {/* <input className={styles.inputBox} type='phone'  value={formData.mobNumber || ""} placeholder='Phone number *' onChange={(e)=>onInputChange(e,'mobNumber')}  /> */}
-                {/* <span>Error</span> */}
+            {errors.mobNumber && <span className={styles.errorMsg}>{errors.mobNumber}</span>}
             </div>
             <div>
                 <Input lassName={styles.inputBox} type='email'  value={formData.email || ""} placeHolder='Email ID (ex. abc@gmail.com)' onInputChange={(e)=>onInputChange(e,'email')}  />
-                {/* <span>Error</span> */}
+                {errors.email && <span className={styles.errorMsg}>{errors.email}</span>}
             </div>
             <div>
                 <Input className={styles.inputBox} type='password'  value={formData.password || ""} placeHolder='Set password' onInputChange={(e)=>onInputChange(e,'password')} />
-                {/* <span>Error</span> */}
+                {errors.password && <span className={styles.errorMsg}>{errors.password}</span>}
             </div>
         </div>
     )
@@ -50,72 +75,40 @@ const SignupForm = ({setFormData={},formData={}}) => {
 
 export default function SignupCard() {
     const router = useRouter();
+    const [ formData , setFormData] = useState({});
+    const [ errors, setErrors] = useState({});  
 
-    const [ formData , setFormData] = useState({})
-    const [ userEmail , setUserEmail ] = useState("");
-    const [ password , setPassword ] = useState("");
-    const [inputs, setInputs] = useState({});
-   
-
-    const onLogin = async() =>{
-        try {
-
-          const res = await fetch('/api/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: 'Bearer ' + "didToken",
-            },
-            body:JSON.stringify({
-                'username':userEmail,
-                'password':password
-            })
-          })
-          if (res.status === 200) {
-            router.push('/')
-          } else {
-            throw new Error(await res.text())
-          }
-        } catch (error) {
-          console.error('An unexpected error happened occurred:', error)
-          setErrorMsg(error.message)
-        }
-    }
-
-
-    const onEmailChange = (e) => {
-        setUserEmail(e.target.value)
-    }
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value)
-    }
   
 
 
       const onSignup = async() =>{
-        try {
-
-          const res = await fetch('/api/signup', {
-            method: 'POST',
-            body:JSON.stringify(formData)
-          })
-          if (res.status === 200) {
-            window.location.href = '/'
-          } else {
-            throw new Error(await res.text())
+        const validationErrors = validateForm(formData);
+        if (Object.keys(validationErrors).length === 0) {
+            try {
+              const res = await fetch('/api/signup', {
+                method: 'POST',
+                body:JSON.stringify(formData)
+              })
+            if (res.status === 200) {
+              window.location.href = '/'
+            } else {
+              throw new Error(await res.text())
+            }
+          } catch (error) {
+            console.error('An unexpected error happened occurred:', error)
           }
-        } catch (error) {
-          console.error('An unexpected error happened occurred:', error)
+        } else {
+          setErrors(validationErrors);
         }
+
       }
 
-      console.log("formData",formData)
 
       return (
         <div className={styles.signUpCardWrapper}>
           <div className={styles.signUpTxt}>Create an account</div>
           <div className={styles.descTxt}>Create or login to enjoy exclusive benefits.</div>
-            <SignupForm setFormData={setFormData} formData={formData}/>
+            <SignupForm setFormData={setFormData} formData={formData} errors={errors}/>
             <div className={styles.createAccountBtn} onClick={onSignup}>Create account</div>
             <div className={styles.loginTxt} onClick={()=> router.push('/login')}>Already have an account ? <span className={styles.loginSubTxt} >Login</span></div>
         </div>
