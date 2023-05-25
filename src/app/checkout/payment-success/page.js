4b86cd91f9d14499a72e4a4c471e227b29c7c4e6
gopalan10 <gopal@feelvaleo.com>
2023-05-25 13:31:45 +0530

    added payment success

--- src/app/checkout/payment-success/page.js
+++ src/app/checkout/payment-success/page.js
@@ -3,11 +3,14 @@ import { redirect } from 'next/navigation';
 
 export default async function PaymentSuccess(req,res) {
     const sessionId = req && req.searchParams && req.searchParams['cko-session-id'] || null;
-    console.log("sessionIdsessionId",sessionId)
     if(sessionId){
-        // const data = await fetch(`https://api.kuwa.bevaleo.dev/api/v1/checkout/callback-success?cko-session-id=${sessionId}`);
-        // console.log("DATA",data);
-        // redirect("/payment/success")
+        const data  =  await fetch(`https://api.kuwa.bevaleo.dev/api/v1/checkout/callback-success?cko-session-id=${sessionId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        redirect("/payment/success");
     }
     
 
