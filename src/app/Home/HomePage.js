4054f68c0521fc9d26b51952bae5ef41da79efcd
gopalan10 <gopal@feelvaleo.com>
2023-05-28 16:31:46 +0530

    fixed

--- src/app/Home/HomePage.js
+++ src/app/Home/HomePage.js
@@ -1,7 +1,6 @@
 'use client';
 import { useEffect, useState } from "react";
 import Banner from './Banner/Banner';
-import VideoBanner from "./VideoBanner/VideoBanner";
 import Header from '@/components/Header/Header';
 import AssuredInfo from './AssuredInfo/AssuredInfo';
 import BestSellingProduct from './BestSellingProduct/BestSellingProduct';
@@ -24,7 +23,6 @@ export default async function Home(props) {
             <div className={styles.homePageContainer}>
                 <div className={styles.mainBanner}>
                     <Banner />
-                    {/* <VideoBanner /> */}
                 </div>
                 <div className={styles.mainContainer}>
                     <Header />
