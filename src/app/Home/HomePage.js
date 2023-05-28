5f01c6de95a326860e7e04e59d6b58c0dcac108d
gopalan10 <gopal@feelvaleo.com>
2023-05-28 12:09:49 +0530

    added home page banners

--- src/app/Home/HomePage.js
+++ src/app/Home/HomePage.js
@@ -1,5 +1,5 @@
 'use client';
-import React,{useState} from 'react';
+import { useEffect, useState } from "react";
 import Banner from './Banner/Banner';
 import Header from '@/components/Header/Header';
 import AssuredInfo from './AssuredInfo/AssuredInfo';
@@ -10,60 +10,40 @@ import ProductSlider from '@/components/ProductSlider/ProductSlider';
 import MedicalExpert from './MedicalExpert/MedicalExpert';
 import Footer from '@/components/Footer/Footer';
 import Loader from '@/components/Loader/Loader';
+import styles from './home-page.module.scss';
 
 export default async function Home(homePageData) {
-
-  const {kuwaUsps=[],data=[],brandUMustTry=[] } = homePageData || {};
-//   const [y, setY] = useState(0);
+    const {kuwaUsps=[],data=[],brandUMustTry=[] } = homePageData || {};
 
 
-//   useEffect(() => {
-  
-//     window.addEventListener("scroll", handleNavigation);
-  
-//     return () => {
-//       window.removeEventListener("scroll", handleNavigation);
-//     };
-//   }, []);
 
-//   const handleNavigation = useCallback(
-//     e => {
-//       const window = e.currentTarget;
-//       if (y > window.scrollY) {
-//         console.log("scrolling up");
-//       } else if (y < window.scrollY) {
-//         console.log("scrolling down");
-//       }
-//     //   setY(window.scrollY);
-//     }, [y]
-//   );
-  
+    return(
 
-//   console.log("yy",y)
-  return (
-    <>
-    <div>
-
- 
-        <Banner />
-        <Header />
-        <AssuredInfo assuredInfo={kuwaUsps} />
-        <BestSellingProduct data={data} />
-        <BrandMustTry data={brandUMustTry}/>
-        <SecondaryBanner />
-        {
-          data.map((data,index)=>{
-            console.log("datatad",data)
-            return(
-              <ProductSlider data={data} index={index}/>
-            )
-          })
-        }
-        <MedicalExpert />
-        <Footer />
-        <Loader />
+        <div className={styles.homePageWrapper}>
+            <div className={styles.homePageContainer}>
+                <div className={styles.mainBanner}>
+                    <Banner />
+                </div>
+                <div className={styles.mainContainer}>
+                    <Header />
+                    <AssuredInfo assuredInfo={kuwaUsps} />
+                    <BestSellingProduct data={data} />
+                    <BrandMustTry data={brandUMustTry}/>
+                    <SecondaryBanner />
+                    {
+                    data.map((data,index)=>{
+                        console.log("datatad",data)
+                        return(
+                        <ProductSlider data={data} index={index}/>
+                        )
+                    })
+                    }
+                    <MedicalExpert />
+                    <Footer />
+                    <Loader />
+                </div>
+            </div>
         </div>
-    </>
-
-  )
+      
+    )
 }
