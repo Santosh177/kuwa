2d09ccb5a9aa310bf3b4155c636221b950dc8b21
gopalan10 <gopal@feelvaleo.com>
2023-05-31 17:28:37 +0530

    fixed

--- src/app/api/update-country/route.js
+++ src/app/api/update-country/route.js
@@ -1,9 +1,26 @@
 
 import { NextResponse } from 'next/server'
 import { setCountryCookie } from '../../../lib/auth-cookies';
-
+import { authHeader } from '../../../lib/auth-cookies';
 export async function POST(request,res) {
     const requestBody = await request.json();
+    const customHeader = await authHeader();
+    const countryData = {
+        country:parseInt(requestBody.countryId)
+    }
+    if(customHeader && customHeader.user){
+        const updateProfileResp = await fetch(`https://api.kuwa.bevaleo.dev/api/v1/customer/${customHeader.user}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              ...customHeader
+            },
+            body:JSON.stringify(countryData)
+          });
+         const updateProfileData = await updateProfileResp.json();
+         console.log("updateProfileData",updateProfileData)
+    }
+ 
      const data =  setCountryCookie(res, requestBody.countryId)
     return NextResponse.json({status:"SUCCESS"})
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,17 @@
 import HomePage from "./Home/HomePage";
-import { authHeader } from "@/lib/auth-cookies";
+import { getCountryCookie} from '../lib/auth-cookies';
 export default async function Home({}) {
-  const customHeader = await authHeader();
+  const countryIdFromCookie = getCountryCookie();
   const homePageData  =  await fetch(`https://api.kuwa.bevaleo.dev/module/home-page`, {
     method: 'GET',
     headers:{
-      ...customHeader
-    },
+    'Content-Type': 'application/json',
+    'country' : parseInt(countryIdFromCookie)
+  },
     next: { revalidate: 0} 
   })
   const homePageDataResp = await homePageData.json();
 
-
-
-
-
-  // const {kuwaUsps=[],data=[],brandUMustTry=[] } = homePageDataResp || {};
 
 
   return (
