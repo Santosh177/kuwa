1cf58d33ca90a98eccda7d446a9e073b1bea9efb
gopalan10 <gopal@feelvaleo.com>
2023-06-02 10:24:32 +0530

    fixed

---
+++ src/app/my/order/cancellation-request/page.js
@@ -0,0 +1,17 @@
+import PageHeader from '@/components/PageHeader/PageHeader';
+import CancelRequest from './CancelRequest/CancelRequest';
+
+
+export default async function CancellationRequest({}) {
+
+
+
+  return (
+    <>
+      <PageHeader headerName="Cancellation Request" />
+      <CancelRequest />
+ 
+    </>
+
+  )
+}
