95dd53cb28fee04f137e276c673f57d49b40876b
gopalan10 <gopal@feelvaleo.com>
2023-06-02 19:07:10 +0530

    update cart item fixed

--- src/app/api/update-cart-item/route.js
+++ src/app/api/update-cart-item/route.js
@@ -4,14 +4,11 @@ import { authHeader } from '../../../lib/auth-cookies';
 export async function POST(request,res) {
     const requestBody = await request.json();
     const customHeader = await authHeader();
-    const updateCartItem = await fetch('https://api.kuwa.bevaleo.dev/api/v1/cart', {
-        method: 'POST',
+    const updateCartItem = await fetch('https://api.kuwa.bevaleo.dev/api/v1/update/cart', {
+        method: 'PATCH',
         headers: customHeader,
         body:JSON.stringify(requestBody)
       });
       const updateCart = await updateCartItem.json();
-
-      console.log("updateCartupdateCart",updateCart)
-
     return NextResponse.json({data: updateCart,status:"SUCCESS"})
 }
