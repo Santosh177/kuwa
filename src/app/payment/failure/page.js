2f8e83295f13786d66d301a7bf5a6e74f1aa2b5f
gopalan10 <gopal@feelvaleo.com>
2023-06-06 11:03:16 +0530

    fixed

--- src/app/payment/failure/page.js
+++ src/app/payment/failure/page.js
@@ -1,8 +1,10 @@
-
+'use client'
+import { useRouter } from 'next/navigation';
 import styles from './pages.module.scss';
 
 export default function PaymentFailure() {
 
+  const router = useRouter();
 
 
   
@@ -11,7 +13,7 @@ export default function PaymentFailure() {
           <img src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/paymentFailure.png' alt=''/>
           <div className={styles.txt}>Payment failed</div>
           <div className={styles.subTxt}>Your payment couldn't go through.Please make sure that your payment details are correct, your card is not expired and/or your account contains sufficient funds.</div>
-          <div className={styles.btn}>Retry</div>
+          <div className={styles.btn} onClick={()=>router.replace('/payment')}>Retry</div>
         </div>
       )
     }
