4930563332cf88fc6c116ac5cc13b15c45641bac
gopalan10 <gopal@feelvaleo.com>
2023-06-06 17:21:46 +0530

    fixed

--- src/app/my/order/cancellation-request/CancelRequest/CancelRequest.js
+++ src/app/my/order/cancellation-request/CancelRequest/CancelRequest.js
@@ -1,37 +1,54 @@
-
+'use client'
+import { useState } from 'react';
 import styles from './cancel-request.module.scss';
 
 
 const CheckBox = ({ isChecked=false }) => {
     return (
       <div className={styles.checkboxContainer}>
-        <input type="checkbox" checked={true} />
+        <input type="checkbox" checked={isChecked} />
         <span className={styles.checkmark}></span>
       </div>
     );
   };
-const ReasonCard = () => {
+const ReasonCard = ({data,onSelect,cancelId}) => {
 
+   const isSelected = data.id == cancelId;
     return(
-        <div className={styles.reasonCard}>
-            <CheckBox />
-            <div className={styles.reasonTxt}>I have changed my mind</div>
+        <div className={styles.reasonCard} onClick={()=>onSelect(data)} style={(isSelected)?{backgroundColor:'#F1FAF8'}:{}}>
+            <CheckBox isChecked={isSelected}/>
+            <div className={styles.reasonTxt}>{data.reason}</div>
         </div>
     )
 }
 
 
-export default async function CancelRequest({}) {
+export default  function CancelRequest({cancelReasonData=[]}) {
+  const [ cancelId , setCancelId ] = useState("");
+
+
+  
+  const onCancelRequest = () => {
+    if(cancelId){
+      alert("cancel request")
+    }
+  }
 
 
 
   return (
     <div className={styles.cancelRequest}>
         <div className={styles.headerTxt}>Reason for cancellation</div>
-        <div>
-            <ReasonCard />  
+        <div className={styles.cancelReasonItemList}>
+          {
+            cancelReasonData.map((data,index)=>{
+              return(
+                <ReasonCard  data={data} onSelect={(data)=>setCancelId(data.id)} cancelId={cancelId}/>  
+              )
+            })
+          }
+          <div className={styles.submitRequest} onClick={onCancelRequest}>Submit Request</div>
         </div>
-
 
     </div>
 
