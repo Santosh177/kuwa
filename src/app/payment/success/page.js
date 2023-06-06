3fba7d19cd48f1260690d0d49bd1a8c511aee0f6
gopalan10 <gopal@feelvaleo.com>
2023-06-06 11:24:11 +0530

    css fixed

--- src/app/payment/success/page.js
+++ src/app/payment/success/page.js
@@ -1,10 +1,12 @@
+'use client'
 import PageHeader from '@/components/PageHeader/PageHeader';
+import { useRouter } from 'next/navigation';
 import styles from './pages.module.scss';
 
 export default function PaymentSuccess() {
 
 
-
+  const router = useRouter();
   
       return (
         <>
@@ -13,7 +15,7 @@ export default function PaymentSuccess() {
           <img src='https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/success.png' alt=''/>
           <div className={styles.txt}>Order placed</div>
           <div className={styles.subTxt}>Thanks for your purchase! Confirmation email with details coming soon. Contact us if you have any questions.</div>
-          <div className={styles.btn}>Continue Shopping</div>
+          <div className={styles.btn} onClick={()=>router.push('/')}>Continue Shopping</div>
         </div>
         </>
       )
