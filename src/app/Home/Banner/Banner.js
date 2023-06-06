63c982e12b191c1c0f7aeb0fa928d23c37029348
gopalan10 <gopal@feelvaleo.com>
2023-06-06 13:25:17 +0530

    banner updated

--- src/app/Home/Banner/Banner.js
+++ src/app/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ export default function Banner() {
   return (
     <>
       <div className={styles.mobBanner}>
-        <img src="https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/top_banner_desktop.png" alt="top-banner"/>
+        <img src="https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/top_banner_mobile.png" alt="top-banner"/>
       </div>
       <div className={styles.desktopBanner}>
         <img src="https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/top_banner_desktop.png" alt="top-banner"/>
