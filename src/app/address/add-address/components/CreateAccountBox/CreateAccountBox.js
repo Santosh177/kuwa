d0ec13626fd755e70f52e224db1e7e2963186eab
gopalan10 <gopal@feelvaleo.com>
2023-06-07 15:02:36 +0530

    fixed

--- src/app/address/add-address/components/CreateAccountBox/CreateAccountBox.js
+++ src/app/address/add-address/components/CreateAccountBox/CreateAccountBox.js
@@ -1,19 +1,19 @@
 'use client';
-
+import { useRouter,useSearchParams } from 'next/navigation';
 import styles from './create-account-box.module.scss';
 
 
 export default function CreateAccountBox() {
 
-
+    const router = useRouter();
 
   
       return (
         <>
-          <div className={styles.createAccountBox}> 
+          <div className={styles.createAccountBox} onClick={()=>router.push('/sign-up?referer=/address/add-address')}> 
                 <div className={styles.createAccountItem}>
                     <div className={styles.newHereTxt}>New here ?</div>
-                    <div className={styles.createAccountTxt}>Create an account and enjoy 20AED off your first order!</div>
+                    <div className={styles.createAccountTxt}>Create an account</div>
                 </div>
                 <div className={styles.createAccountBtn}>Create account</div>
           </div>
