2bcec8889d13420de2f36760298331fef699dbf8
gopalan10 <gopal@feelvaleo.com>
2023-06-08 15:26:36 +0530

    fixed css

--- src/app/cart/cartPage.js
+++ src/app/cart/cartPage.js
@@ -138,6 +138,7 @@ export default  function Cart({cartData}) {
      
   
 
+    const totalPrice =(priceDetails && priceDetails.totalAmount)? priceDetails.currency +" "+priceDetails.totalAmount :""
  
       return (
         <>
@@ -160,7 +161,7 @@ export default  function Cart({cartData}) {
               <CompanyInfo />
             </div>
           </div>
-          <PaymentFooterBtn btnName="Proceed to checkout" totalPrice={priceDetails.currency+" "+priceDetails.totalAmount} onProceed={onProceed} />
+          <PaymentFooterBtn btnName="Proceed to checkout" totalPrice={totalPrice} onProceed={onProceed} />
           <Loader isShow={isLoading}/>
         </>
       )
--- src/components/PaymentFooterBtn/PaymentFooterBtn.js
+++ src/components/PaymentFooterBtn/PaymentFooterBtn.js
@@ -9,7 +9,7 @@ export default function PatmentFooterBtn({btnName="",totalPrice="",onProceed={}}
         <div className={styles.paymentFooterbtn} >
             <div className={styles.paymentFooterBtnContainer}>
                 <div className={styles.paymentInfo}>
-                    <div className={styles.txt}>Total : <span className={styles.price}>{totalPrice}</span></div>
+                    <div className={styles.txt}>Total : <span className={styles.price}>{(totalPrice != undefined)?totalPrice:""}</span></div>
                     <div className={styles.subTxt}>View price details</div>
                 </div>
                 <div className={styles.btn} onClick={()=>onProceed()}>{btnName}</div>
