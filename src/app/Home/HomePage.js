da071afcdecb700fa301e75b95875a04c086a9d6
gopalan10 <gopal@feelvaleo.com>
2023-06-12 12:41:57 +0530

    fixed

--- src/app/Home/HomePage.js
+++ src/app/Home/HomePage.js
@@ -6,7 +6,7 @@ import BrandMustTry from './BrandMustTry/BrandMustTry';
 import SecondaryBanner from './SecondaryBanner/SecondaryBanner';
 import ProductSlider from '@/components/ProductSlider/ProductSlider';
 import MedicalExpert from './MedicalExpert/MedicalExpert';
-import CustomerSay from './CustomerSay/CustomerSay';
+// import CustomerSay from './CustomerSay/CustomerSay';
 import Footer from '@/components/Footer/Footer';
 import Loader from '@/components/Loader/Loader';
 import VideoBanner from './VideoBanner/VideoBanner';
@@ -42,7 +42,7 @@ export default async function Home(homePageData) {
                         })
                     }
                     <MedicalExpert />
-                    <CustomerSay />
+                    {/* <CustomerSay /> */}
                     <Footer />
                     <Loader />
                     </div>
