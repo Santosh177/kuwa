de718cc2f02bc52dd23ea4884e08fc01e435581d
gopalan10 <gopal@feelvaleo.com>
2023-06-22 16:11:43 +0530

    fixed

--- src/app/api/update-country/route.js
+++ src/app/api/update-country/route.js
@@ -11,7 +11,7 @@ export async function POST(request,res) {
         country:parseInt(requestBody.countryId)
     }
     if(customHeader && customHeader.user){
-        const updateProfileResp = await fetch(`https://api.kuwa.bevaleo.dev/api/v1/customer/${customHeader.user}`, {
+        const updateProfileResp = await fetch(`https://api.kuwa.bevaleo.dev/api/v1/customer/country/${customHeader.user}?country=${parseInt(requestBody.countryId)}`, {
             method: 'PUT',
             headers: {
               'Content-Type': 'application/json',
