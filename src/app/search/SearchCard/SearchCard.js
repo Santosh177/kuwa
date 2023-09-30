cd7670f16b162f52579f857fdad300d7a580b755
gopalan10 <gopal@feelvaleo.com>
2023-09-30 11:20:30 +0530

    fixed

--- src/app/search/SearchCard/SearchCard.js
+++ src/app/search/SearchCard/SearchCard.js
@@ -5,10 +5,10 @@ import { useRouter,usePathname } from 'next/navigation';
 
 export default function SearchCard({searchData={}}) {
     const router = useRouter();
-    const { productImage="", productName="", id="" } = searchData || {};
+    const { productImage="", productName="", id="", seoUrl="" } = searchData || {};
 
     return (
-        <div className={styles.searchCard} onClick={()=>router.push(`/products/${id}`)} id="search-container">
+        <div className={styles.searchCard} onClick={()=>router.push(`/products/${seoUrl}`)} id="search-container">
             <div className={styles.productContainer} id="search-container">
                 <div className={styles.productImage} id="search-container">
                     <img src={productImage}  alt='product-image' id="search-container"/>
