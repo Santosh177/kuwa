44d52eac49bdb0cf9336c3bb7b8714513107a58e
gopalan10 <gopal@feelvaleo.com>
2023-10-10 12:32:49 +0530

    fixed

--- src/services/index.js
+++ src/services/index.js
@@ -8,8 +8,14 @@ export const addToCart = async(data) =>{
         },
         body:JSON.stringify(data)
       })
-    const addToCartRespData = await addToCartResp.json();
-    return addToCartRespData;
+      try {   
+        const addToCartRespData = await addToCartResp.json();
+        return addToCartRespData;
+        
+      } catch (error) {
+        return {}
+      }
+ 
 }
 
 export const updateCartItem = async(data) =>{
@@ -20,8 +26,14 @@ export const updateCartItem = async(data) =>{
         },
         body:JSON.stringify(data)
       })
-    const updateCartItemData = await updateCartItemResp.json();
-    return updateCartItemData;
+
+    try {
+      const updateCartItemData = await updateCartItemResp.json();
+      return updateCartItemData;
+    } catch (error) {
+      return {}
+    }
+
 }
 
 
@@ -33,8 +45,13 @@ export const deleteCartItem = async(data) =>{
         },
         body:JSON.stringify(data)
       })
-    const deleteCartItemData = await deleteCartItemResp.json();
-    return deleteCartItemData;
+      try {
+        const deleteCartItemData = await deleteCartItemResp.json();
+        return deleteCartItemData;
+      } catch (error) {
+        return {}
+      }
+    
 }
 
 export const getCartItem = async() =>{
