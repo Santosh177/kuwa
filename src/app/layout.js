cdb93ddb5a88a0d29b25a88dde5ef630407d0fb3
gopalan10 <gopal@feelvaleo.com>
2023-10-16 15:33:08 +0530

    fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -84,7 +84,7 @@ export default async function RootLayout({ children }) {
   }
 
 
-
+  const isProd = (process.env.NODE_ENV === 'pre-prod') || (process.env.NODE_ENV === 'prod')
 
 
 
@@ -96,7 +96,7 @@ export default async function RootLayout({ children }) {
       <script type="text/javascript" src="https://checkout.tabby.ai/tabby-promo.js" async></script>
       <Script src="https://cdn.tamara.co/widget/product-widget.min.js" strategy="lazyOnload" />
       <Script src="/tamara-script.js" strategy="lazyOnload" />
-      <Script src="/clevertap-stage.js" strategy="lazyOnload" />
+      <Script src={isProd?"/clevertap-prod.js":"/clevertap-stage.js"} strategy="lazyOnload" />
       
       <CountryListProvider countryList={countryList}>
         <CountryProvider countryCode={"AE"} selectedCountryData={selectedCountryData} countryList={countryList}>
