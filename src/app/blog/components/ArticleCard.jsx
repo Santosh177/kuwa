de06b4ddc16605aca175ada57284890252565f56
abhishek-feelvaleo <abhishek@feelvaleo.com>
2023-11-01 19:41:55 +0530

    error-module-notfound

--- src/app/blog/components/ArticleCard.jsx
+++ src/app/blog/components/ArticleCard.jsx
@@ -1,5 +1,5 @@
 "use client"
-import "./ArticleCard.scss";
+import "./articleCard.scss";
 import { useRouter } from 'next/navigation';
 
 
--- src/app/blog/components/CategoriesModal.jsx
+++ src/app/blog/components/CategoriesModal.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import "./blogcategories.scss"
+import "./blogCategories.scss"
 function CategoriesModal({setIsLoading,setArticleData,categoriesList,setIsShowModal}) {
    function hideCategotiesModal(){
     setIsShowModal(false)
