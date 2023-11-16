5a9c216d5648a1df3c3849ff15fd80f96f478e70
gopalan10 <gopal@feelvaleo.com>
2023-11-16 17:23:47 +0530

    fixed

--- src/app/my/orders/EmptyOrders/EmptyOrders.js
+++ src/app/my/orders/EmptyOrders/EmptyOrders.js
@@ -4,8 +4,6 @@ import { useRouter } from 'next/navigation';
 import EmptyOrderAnimation from './empty_bag.json'
 import Lottie from "react-lottie";
 import styles from './empty-order.module.scss';
-import ProductPageNotFound from './product-page-no-found.json'
-console.log("santo",EmptyOrderAnimation)
 
 
 export default function EmptyOrder(){
@@ -14,13 +12,14 @@ export default function EmptyOrder(){
     const defaultOptions ={
         loop: true,
         autoplay: true,
-        animationData: ProductPageNotFound,
+        animationData: EmptyOrderAnimation,
     }
     
     return (
         <>
             <div className={styles.emptyOrderWrapper}>
                 <div>  <Lottie options={defaultOptions}/></div>
+           
                 <div className={styles.txt}>Oops! No Orders</div>
                 <div className={styles.subTxt}>It seems like there are no orders to display at the moment.</div>
                 <div className={styles.btn} onClick={()=>router.push("/")}>Continue Shopping</div>
