0141593e3fc27a1cd6c6a1a662a4972f711f1719
gopalan10 <gopal@feelvaleo.com>
2023-12-13 19:04:56 +0530

    fixeD

--- src/app/products/component/productDetail/subComponents/productPricingSection.jsx
+++ src/app/products/component/productDetail/subComponents/productPricingSection.jsx
@@ -27,6 +27,106 @@ const ProductPricingSection = ({ pricingSectionVariables, isAddedToCart=false, o
         }
     },[(typeof window !== "undefined") && window && window.TamaraProductWidget,finalPrice,currency,tamaraConfig])
 
+
+    const onApplePay = () => {
+     
+        const applePaySupportednetworks = "visa, mastercard, amex";
+        let request = {
+          merchantCapabilities: ['supports3DS'],
+          supportedNetworks: applePaySupportednetworks.split(", "),
+          countryCode: "AE" || "",
+          currencyCode:  "AED" || "",
+          total: { label: "For " + "Multiple_Package", amount: 500 },
+          "shippingType": "shipping",
+          "requiredBillingContactFields": [
+              "postalAddress",
+              "name"
+          ],
+          "requiredShippingContactFields": [
+              "postalAddress",
+              "name",
+              "phone",
+              "email"
+          ],
+          "lineItems": [
+              {
+                  "label": "Sales Tax",
+                  "amount": "0.00"
+              },
+              {
+                  "label": "Shipping",
+                  "amount": "0.00"
+              }
+          ],
+        };
+        appleSession = new ApplePaySession(3, request);
+        appleSession.begin();
+        appleSession.onshippingmethodselected = function (event) {
+            console.log("eventevent",event)
+            var newTotal = {
+                type: 'final',
+                label: "config.shop.shop_name",
+                amount: 500
+            }
+            var newLineItems = [
+                {
+                type: 'final',
+                label: 'Subtotal',
+                amount: 500
+                },
+                {
+                type: 'final',
+                label: "event.shippingMethod.label,",
+                amount: 500
+                }
+            ]
+            appleSession.completeShippingMethodSelection(
+                ApplePaySession.STATUS_SUCCESS,
+                newTotal,
+                newLineItems
+            )
+        }
+        appleSession.onvalidatemerchant = async (event) => {
+          const appleValidationURL = event && event.validationURL;
+          // alert("va"+JSON.stringify(appleValidationURL))
+          const validateData = {"apple_url":appleValidationURL,"merchant_name":'checkout'};
+          console.log("validateDatavalidateData",validateData)
+          const validateSessionResp  =  await fetch('/api/validate-apple-pay-session', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(validateData)
+          })
+          const validateSessionData = await validateSessionResp.json();
+          console.log("validateSessionData",validateSessionData)
+          const getValidateSession = validateSessionData['session_response'];
+          if (getValidateSession) {
+            appleSession.completeMerchantValidation(getValidateSession);
+          }
+          appleSession.onpaymentauthorized = async (event) => {
+              console.log("payment TOkenn",event)
+              const appleToken = event.payment.token;
+              const decryptAppleTokenResp  =  await fetch('/api/decrypt-apple-token', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body:JSON.stringify(appleToken)
+              })
+              const decryptAppleToken = await decryptAppleTokenResp.json();
+              const getCheckoutToken = decryptAppleToken.token_response;
+              console.log("getCheckoutToken",getCheckoutToken)
+              if (getCheckoutToken) {
+                let data = {
+                  token: getCheckoutToken.token
+                } 
+               
+              }
+          }
+        }
+    }
+
     const tamaraMinAmount = tamaraConfig && Object.keys(tamaraConfig).length> 0 ? tamaraConfig&&tamaraConfig[0] && tamaraConfig[0].min_limit && tamaraConfig[0].min_limit.amount:0
     const tamaraMaxAmount = tamaraConfig && Object.keys(tamaraConfig).length> 0 ? tamaraConfig&&tamaraConfig[0] && tamaraConfig[0].max_limit && tamaraConfig[0].max_limit.amount:0
 
@@ -51,6 +151,10 @@ const ProductPricingSection = ({ pricingSectionVariables, isAddedToCart=false, o
                 <div className={styles.addToCart} onClick={() => handelAddToCart()} ><span>Add to Cart</span></div>
                }
                 <div className={styles.buyNow} onClick={() => handelBuyNow()} ><span>Buy Now</span></div>
+            </div>
+
+            <div style={{marginTop:'24px',cursor:'pointer'}} onClick={()=>onApplePay()}>
+               <img style={{width:'100%'}} src="https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/default.png" alt="apple-pay"></img>
             </div>
             {/* <div className={styles.shareConatiner}>
                 <div className={styles.Share} >Share:</div>
