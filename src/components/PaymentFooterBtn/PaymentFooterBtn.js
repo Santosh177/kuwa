0a20a48cb4ef0a039f6a5adb840d9f0de38b44a1
gopalan10 <gopal@feelvaleo.com>
2023-12-15 19:30:16 +0530

    fixed

--- src/components/PaymentFooterBtn/PaymentFooterBtn.js
+++ src/components/PaymentFooterBtn/PaymentFooterBtn.js
@@ -1,7 +1,7 @@
 
 import styles from './payment-footer-btn.module.scss';
 
-export default function PatmentFooterBtn({btnName="",totalPrice="",onProceed={}}) {
+export default function PatmentFooterBtn({btnName="",totalPrice="",onProceed={},onHandleApplePay={}}) {
 
 
   
@@ -11,6 +11,9 @@ export default function PatmentFooterBtn({btnName="",totalPrice="",onProceed={}}
                 <div className={styles.paymentInfo}>
                     <div className={styles.txt}>Total : <span className={styles.price}>{(totalPrice != undefined)?totalPrice:""}</span></div>
                     <div className={styles.subTxt}>View price details</div>
+                </div>
+                <div style={{cursor:'pointer',height:'48px'}} onClick={()=>onHandleApplePay()}>
+                  <img style={{height:'100%',width:'100%'}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/assets/default.png' alt='apple-pay' />
                 </div>
                 <div className={styles.btn} onClick={()=>onProceed()}>{btnName}</div>
             </div>
