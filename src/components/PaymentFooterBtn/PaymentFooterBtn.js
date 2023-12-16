99e0fa862cc5ddf7748609b539a3e544d1625bcd
gopalan10 <gopal@feelvaleo.com>
2023-12-16 10:41:06 +0530

    fixed

--- src/components/PaymentFooterBtn/PaymentFooterBtn.js
+++ src/components/PaymentFooterBtn/PaymentFooterBtn.js
@@ -12,10 +12,12 @@ export default function PatmentFooterBtn({btnName="",totalPrice="",onProceed={},
                     <div className={styles.txt}>Total : <span className={styles.price}>{(totalPrice != undefined)?totalPrice:""}</span></div>
                     <div className={styles.subTxt}>View price details</div>
                 </div>
-                <div style={{cursor:'pointer',height:'48px'}} onClick={()=>onHandleApplePay()}>
-                  <img style={{height:'100%',width:'100%'}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/assets/default.png' alt='apple-pay' />
+                <div className={styles.paymentBtnContainer}>
+                  { window && window.ApplePaySession &&<div  className={styles.applePayContainer} style={{cursor:'pointer',height:'48px',}} onClick={()=>onHandleApplePay()}>
+                    <img style={{height:'100%',width:'100%'}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/assets/default.png' alt='apple-pay' />
+                  </div>}
+                  <div className={styles.btn} onClick={()=>onProceed()}>{btnName}</div>
                 </div>
-                <div className={styles.btn} onClick={()=>onProceed()}>{btnName}</div>
             </div>
         </div>
       )
