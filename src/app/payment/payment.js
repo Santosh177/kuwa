7bf85eed8d10c5e1c8bbed2f740c31b63034af1c
gopalan10 <gopal@feelvaleo.com>
2023-12-28 15:37:13 +0530

    fixed

--- src/app/payment/payment.js
+++ src/app/payment/payment.js
@@ -527,6 +527,7 @@ export default function Payment({cartData,paymentModes,tamaraConfig}) {
 
     const onProceed = () => {
       if(selectedPaymentMethod =="CHECKOUT_CARD"){
+        setIsLoader(true);
         Frames.submitCard()
       }else if(selectedPaymentMethod === "APPLE_PAY"){
         const applePaySupportednetworks = "visa, mastercard, amex";
