3148442b442bb39da81f4bcbaa5b1cafb6013318
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-01-16 11:10:28 +0530

    script for prod

--- public/spin-wheel.js
+++ public/spin-wheel.js
@@ -3,7 +3,7 @@ function spinWheel(){
 
   (function(w,i,d,g,e,t,s){t=i.createElement(g);				
 		t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t,s);				
-	})(window,document,'_lkda','script','//app.getwoohoo.com/33626/script.js');
+	})(window,document,'_lkda','script','//sfo2.digitaloceanspaces.com/woohoo/public/script/33495/script.js');
 
 
 }
