9c47ba84c275a8d9c766b1b3530d89b2cc243422
vivekgopalan <vivek@feelvaleo.com>
2024-01-16 11:57:31 +0530

    removed apple pay btn

--- src/components/PaymentFooterBtn/PaymentFooterBtn.js
+++ src/components/PaymentFooterBtn/PaymentFooterBtn.js
@@ -13,9 +13,9 @@ export default function PatmentFooterBtn({isApplePaySession=false,btnName="",tot
                     <div className={styles.subTxt} onClick={()=>showViewDetails()}>View price details</div>
                 </div>
                 <div className={styles.paymentBtnContainer}>
-                  {isApplePaySession && <div  className={styles.applePayContainer} style={{cursor:'pointer',height:'48px',}} onClick={()=>onHandleApplePay()}>
+                  {/* {isApplePaySession && <div  className={styles.applePayContainer} style={{cursor:'pointer',height:'48px',}} onClick={()=>onHandleApplePay()}>
                     <img className={styles.applePayImg} style={{height:'100%'}} src='https://production-website-builds.s3.ap-south-1.amazonaws.com/assets/default.png' alt='apple-pay' />
-                  </div>}
+                  </div>} */}
                   <div className={[styles.btn,(!isApplePaySession)?styles.btnTwo:""].join(" ")}   onClick={()=>onProceed()}>{btnName}</div>
                 </div>
             </div>
