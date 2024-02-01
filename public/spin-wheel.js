de3095cfe35104811cd1ec9faa879de7cd4d609b
vivekgopalan <vivek@feelvaleo.com>
2024-02-01 19:53:33 +0530

    fixe

--- public/spin-wheel.js
+++ public/spin-wheel.js
@@ -3,7 +3,7 @@ function spinWheel(){
 
   (function(w,i,d,g,e,t,s){t=i.createElement(g);				
 		t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t,s);				
-	})(window,document,'_lkda','script','//sfo2.digitaloceanspaces.com/woohoo/public/script/33495/script.js');
+	})(window,document,'_lkda','script','//www.wheelofpopups.com/api/v12634/widget.js');
 
 
 }
