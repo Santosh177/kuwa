6dfa151214c4640e2c216450a791a8ef5fc24854
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-02-27 11:29:28 +0530

    updated code

--- src/components/ReviewCard/reviewCard.jsx
+++ src/components/ReviewCard/reviewCard.jsx
@@ -3,8 +3,7 @@ import React from "react";
 import style from "./ReviewCard.module.scss"
 const ReviewCard = ({item}) => {
  
-    const { reviewBody = "", headLine = "", customerName = "", id = "", rating= "" } = item || {}
-    console.log("djsj",item)
+    const { reviewBody = "", headLine = "", customerName = "", id = "", rating= "",isActive } = item || {}
     const getStarImage = (index) => {
         if (index < rating) {
           return "https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/stars.png"; 
@@ -26,10 +25,12 @@ const ReviewCard = ({item}) => {
       };
     
     return (
-        <div className={style.reviewCardConatiner}>
+      <>
+      {isActive &&  <div className={style.reviewCardConatiner}>
             <div className={style.reviewStarConatiner}>
             {renderStars()}
             </div>
+            <div className={style.heading}>{headLine}</div>
             <div className={style.reviewBody}>{reviewBody}</div>
             <div className={style.bottomContainer}>
                 <div className={style.customer}>
@@ -42,7 +43,8 @@ const ReviewCard = ({item}) => {
                
             </div>
         </div>
-       
+    }
+    </>
     )
 }
 
