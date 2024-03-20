78f9921885989eb1a20d607e7f223bb4b112105a
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-03-20 12:32:12 +0530

    updated code

--- src/app/products/component/productDetail/subComponents/productPricingSection.jsx
+++ src/app/products/component/productDetail/subComponents/productPricingSection.jsx
@@ -443,7 +443,7 @@ const ProductPricingSection = ({ pricingSectionVariables, isAddedToCart=false, o
 
 
             
-           { window && window.ApplePaySession && <div style={{marginTop:'24px',cursor:'pointer'}} onClick={()=>onHandleApplePay()}>
+           { window && window.ApplePaySession && <div style={{marginTop:'24px',cursor:normalInventory <=0 || selectedVariantQuantity<=0 ? 'not-allowed':'pointer'}} disabled={normalInventory <=0 || selectedVariantQuantity<=0 } onClick={()=>onHandleApplePay()}>
                <img style={{width:'100%'}} src="https://production-website-builds.s3.ap-south-1.amazonaws.com/kuwa/default.png" alt="apple-pay"></img>
             </div>}
             { window && window.ApplePaySession && prePaidDiscount > 0 && <div className={styles.prepaidDiscount}>Extra {prePaidDiscount}% Off</div>}
