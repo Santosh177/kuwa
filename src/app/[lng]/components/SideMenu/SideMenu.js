a1ef41de17ddd01ac13aa69da27cc74c076376da
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-06-04 11:08:25 +0530

    updated

--- src/app/[lng]/components/SideMenu/SideMenu.js
+++ src/app/[lng]/components/SideMenu/SideMenu.js
@@ -32,7 +32,7 @@ const SideMenuData = ({title="" , data=[],onclose={},onBack={}}) => {
                         return
                     return(
                         <>
-                            <div onClick={()=>window.location.href =`/collections?category=${ isArabic ? encodeURIComponent(data.descriptionArabic) :   encodeURIComponent(data.name)}`} className={styles.item} key={index}>{data.name}</div>
+                            <div onClick={()=>window.location.href =`/collections?category=${encodeURIComponent(data.name)}`} className={styles.item} key={index}>{isArabic ? data.nameArabic :data.name}</div>
                             <div className={styles.horizontalLine}></div>
                         </>
                     )
@@ -228,9 +228,9 @@ const SideMenu = ({onclose={},sideMenuData=[]}) => {
           })
           const getProductTypeData = await getProductTypeRes.json();
           console.log("getProductTypeData",getProductTypeData);
-          getProductTypeData['list'].map((data,index)=>{
-            const {description={} , id="" } = data || {}
-            childMenuData.push({name:description && isArabic ? description.nameArabic : description.name,id:id})
+          getProductTypeData.map((data,index)=>{
+            const {collectionTitle="" , collectionId="" ,collectionTitleArabic} = data || {}
+            childMenuData.push({ name:collectionTitle,id:collectionId,nameArabic:collectionTitleArabic})
           })
           setChildMenuData(childMenuData)
     }
--- src/app/api/product-types/route.js
+++ src/app/api/product-types/route.js
@@ -6,7 +6,7 @@ export async function POST(request,res) {
   const { superType="" } = requestBody || {}
   console.log("requestBody",requestBody)
   const customHeader = await authHeader();
-  const getProductTypes =  await fetch(`${process.env.BACKEND_END_POINT_URL}/api/v1/product/types/website/all?super=${superType}`, {
+  const getProductTypes =  await fetch(`${process.env.BACKEND_END_POINT_URL}/api/v1/active/collection?super=${superType}`, {
     method: 'GET',
     headers: customHeader,
     cache: 'no-store'
