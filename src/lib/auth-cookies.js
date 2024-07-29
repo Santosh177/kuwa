c7c200b4211c3d2d03532d8374d980de27b89637
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-07-29 18:48:35 +0530

    fixed

--- src/lib/auth-cookies.js
+++ src/lib/auth-cookies.js
@@ -97,7 +97,7 @@ export const authHeader = async() =>{
   const token = cookies().get('token')
   const user = cookies().get('userId');
   const country = cookies().get('countryId');
-  const language = cookies().get('language_code').value;
+  const language = cookies().get('language_code');
 
  
   if(token && token.value){
@@ -107,7 +107,7 @@ export const authHeader = async() =>{
         'country':country &&parseInt(country.value) || 8,
         'Authorization':"Bearer "+token.value,
         'user':parseInt(user.value),
-        'preferredLanguage':language || 'en'
+        'preferredLanguage':language && language.value || 'en'
       }
     )
   }else{
@@ -130,7 +130,7 @@ export const authHeader = async() =>{
       'Content-Type': 'application/json',
       'country' : country && parseInt(country.value) || 8,
       'device':cookies().get("deviceID").value || "",
-      'preferredLanguage':language || 'en'
+      'preferredLanguage':language && language.value || 'en'
     }
 
     if(userId){
