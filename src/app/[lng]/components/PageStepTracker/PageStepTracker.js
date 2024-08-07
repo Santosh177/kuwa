aea381c501d7f1e4d6f29b78078acb8a7ef6deb6
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-08-07 19:09:23 +0530

    updated code

--- src/app/[lng]/components/PageStepTracker/PageStepTracker.js
+++ src/app/[lng]/components/PageStepTracker/PageStepTracker.js
@@ -35,26 +35,27 @@ const PageInfoTicker = ({stepCount=0}) => {
 
 
     return(
-    <div className={styles.MainContainer}>
-      <div className={styles.StepContainer} >
-        {steps.map(({ step, label,icon,icon_white, tickIcon }) => (
-          <div className={styles.StepWrapper} key={step}>
-            <div className={ 
-              isArabic ? 
-              [styles.StepStyleAr,(step==2)?styles.StepStyle3:"", (step<=stepCount)?styles.StepStyleActive:'' , (step + 1 <= stepCount)?styles.StepStyleActiveTracker:''].join(" ")
-              :
-              [styles.StepStyle,(step==2)?styles.StepStyle3:"", (step<=stepCount)?styles.StepStyleActive:'' , (step + 1 <= stepCount)?styles.StepStyleActiveTracker:''].join(" ")}>
-                <div className={styles.StepCount}>
-                    <img className={`${styles.stepIcon} ${isArabic ? styles['stepIcon-ar'] : ''}`} src={(step<stepCount)?tickIcon:(step<=stepCount)?icon_white:icon} />
-                </div>
-            </div>
-            <div className={styles.StepsLabelContainer}>
-              <div className={styles.StepLabel} key={step}>{label}</div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      <></>
+    // <div className={styles.MainContainer}>
+    //   <div className={styles.StepContainer} >
+    //     {steps.map(({ step, label,icon,icon_white, tickIcon }) => (
+    //       <div className={styles.StepWrapper} key={step}>
+    //         <div className={ 
+    //           isArabic ? 
+    //           [styles.StepStyleAr,(step==2)?styles.StepStyle3:"", (step<=stepCount)?styles.StepStyleActive:'' , (step + 1 <= stepCount)?styles.StepStyleActiveTracker:''].join(" ")
+    //           :
+    //           [styles.StepStyle,(step==2)?styles.StepStyle3:"", (step<=stepCount)?styles.StepStyleActive:'' , (step + 1 <= stepCount)?styles.StepStyleActiveTracker:''].join(" ")}>
+    //             <div className={styles.StepCount}>
+    //                 <img className={`${styles.stepIcon} ${isArabic ? styles['stepIcon-ar'] : ''}`} src={(step<stepCount)?tickIcon:(step<=stepCount)?icon_white:icon} />
+    //             </div>
+    //         </div>
+    //         <div className={styles.StepsLabelContainer}>
+    //           <div className={styles.StepLabel} key={step}>{label}</div>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
     )
 
   
