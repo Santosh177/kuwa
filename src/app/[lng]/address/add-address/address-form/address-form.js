f5d2cdb440d0d436e379808d40c9dfaa74d16c51
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-08-07 15:19:38 +0530

    updated code

--- src/app/[lng]/address/add-address/address-form/address-form.js
+++ src/app/[lng]/address/add-address/address-form/address-form.js
@@ -15,7 +15,7 @@ import { isMobile, isTablet, isAndroid, isIOS } from 'react-device-detect';
 
 import { useLanguage } from "@/context/languageDetails";
 
-const validatePersonalForm = (formData) => {
+const validatePersonalForm = (formData,selectedCountry) => {
 
   console.log("validatePersonalForm",formData)
     const errors = {};
@@ -31,7 +31,7 @@ const validatePersonalForm = (formData) => {
     if(!formData.mobNumber){
       errors.mobNumber = formData.isArabic ? "رقم الهاتف المحمول مطلوب" :  "Mobile number is required";
     }
-    else if(formData &&  ("mobNoValidation" in formData) && formData.mobNoValidation && !checkInternationalPhone(formData.mobNoValidation)){
+    else if(formData &&  ("mobNoValidation" in formData) && formData.mobNoValidation && !checkInternationalPhone(formData.mobNoValidation,selectedCountry)){
       errors.mobNumber = formData.isArabic ? "رقم الهاتف المحمول غير صحيح" : "Invalid mobile number";
     }
     if(!formData.email){
@@ -479,7 +479,7 @@ export default function AddressForm({onFormData,formData, isEdit=false,onGetForm
       },[userData])
 
       const addressValidation = () => {
-        const validationPersonalInfoErrors = validatePersonalForm(personalInfo);
+        const validationPersonalInfoErrors = validatePersonalForm(personalInfo,selectedCountry);
         const validationShippingErrors = validateShippingAddressForm(shippingAddress,isArabic);
         const validationBillingErrors = validateBillingAddressForm(billngAddress,isArabic);
        
