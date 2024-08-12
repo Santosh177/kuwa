2dcd9a1584a1676e09cba62557a6898efe017895
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-08-12 17:46:12 +0530

    fixed

--- src/app/[lng]/products/component/productDetail/productDetail.jsx
+++ src/app/[lng]/products/component/productDetail/productDetail.jsx
@@ -379,7 +379,7 @@ const ProductDeatil = ({ productData = {},showProductReview }) => {
             devliveryFees =  deliveryFeesConfig.deliveryFee;
             totalAmount = totalAmount + deliveryFeesConfig.deliveryFee
         }
-        let extraDiscount = prePaidDiscount > 0 ? parseFloat(((totalAmount * prePaidDiscount)/100).toFixed(2)) : 0;
+        let extraDiscount = prePaidDiscount > 0 ? parseFloat((((totalAmount - devliveryFees) * prePaidDiscount)/100).toFixed(2)) : 0;
         totalAmount = totalAmount - extraDiscount + customFee
         const applePaySupportednetworks = "visa, mastercard, amex";
         let request = {
