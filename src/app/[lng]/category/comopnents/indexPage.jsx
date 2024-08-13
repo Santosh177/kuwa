157ed4135448e6212e7d90859d469ce137cbd359
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-08-13 19:15:14 +0530

    fixed search api for deal

--- src/app/[lng]/category/comopnents/indexPage.jsx
+++ src/app/[lng]/category/comopnents/indexPage.jsx
@@ -136,23 +136,23 @@ const MainCategory = ({ isDealPage }) => {
     const fetchFilterCollectionData = async () => {
         setIsLoadingProduct(true);
         const { category = [], sort = "", searchKey = "",AVAILABLE="" } = paramsData || {};
-        // let query = "";
+        let dealQueryValue = "";
         let query = {}
         console.log("paramsData",paramsData)
         if (sort && category.length > 0) {
-            // query = `sort_by=${(sort)}&category=${(category.join(','))}`;
+            dealQueryValue = `sort_by=${(sort)}&category=${(category.join(','))}`;
             query = {
                 sort_by: sort,
                 category: category
             }
         } else if (sort && category.length === 0) {
 
-            // query = `sort_by=${(sort)}`;
+            dealQueryValue = `sort_by=${(sort)}`;
             query = {
                 sort_by: sort,
             }
         } else if (!sort && category.length > 0) {
-            // query = `category=${(category.join(','))}`;
+            dealQueryValue = `category=${(category.join(','))}`;
             query = {
                 category: category
             }
@@ -160,12 +160,12 @@ const MainCategory = ({ isDealPage }) => {
 
         if (searchKey) {
             if (!sort && category.length === 0) {
-                // query = `search_key=${(searchKey)}`;
+                dealQueryValue = `search_key=${(searchKey)}`;
                 query = {
                     search_key: searchKey,
                 }
             } else {
-                // query += `&search_key=${(searchKey)}`;
+                dealQueryValue += `&search_key=${(searchKey)}`;
                 query = {
                    ...query,
                     search_key: searchKey,
@@ -175,12 +175,12 @@ const MainCategory = ({ isDealPage }) => {
 
         if (AVAILABLE && AVAILABLE.length > 0 ) {
             if (!sort && category.length === 0 && !searchKey) {
-                // query = `inStock=true`;
+                dealQueryValue = `inStock=true`;
                 query = {
                     inStock: true,
                 }
             } else {
-                // query += `&inStock=true`;
+                dealQueryValue += `&inStock=true`;
                 query = {
                    ...query,
                     inStock: true,
@@ -189,12 +189,12 @@ const MainCategory = ({ isDealPage }) => {
         }
         if(!AVAILABLE || (AVAILABLE && AVAILABLE.length == 0)){
             if (!sort && category.length === 0 && !searchKey) {
-                // query = `inStock=false`;
+                dealQueryValue = `inStock=false`;
                 query = {
                     inStock: false,
                 }
             } else {
-                // query += `&inStock=false`;
+                dealQueryValue += `&inStock=false`;
                 query = {
                    ...query,
                     inStock: false,
@@ -205,25 +205,46 @@ const MainCategory = ({ isDealPage }) => {
         let endpoint = `${process.env.BACKEND_END_POINT_URL}/module/main/search/product?country=${selectedCountry.id}`;
 
         if (isDealPage) {
-            endpoint = `${process.env.BACKEND_END_POINT_URL}/api/v1/deals/${dealSeoUrl}?country_id=${selectedCountry.id}&${query}`;
+            endpoint = `${process.env.BACKEND_END_POINT_URL}/api/v1/deals/${dealSeoUrl}?country_id=${selectedCountry.id}&${dealQueryValue}`;
         }
 
         try {
-            const response = await fetch(endpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body:JSON.stringify(query)
-            });
+            if(isDealPage){
+                const response = await fetch(endpoint, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    // body:JSON.stringify(query)
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                
+                const data = await response.json();
+                console.log("searchData",data)
+                setResponseValue(data);
 
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
             }
-            
-            const data = await response.json();
-            console.log("searchData",data)
-            setResponseValue(data);
+            else{
+                const response = await fetch(endpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body:JSON.stringify(query)
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                
+                const data = await response.json();
+                console.log("searchData",data)
+                setResponseValue(data);
+            }
+           
         } catch (error) {
             console.error('Error fetching data:', error);
             // Handle error or set appropriate state
