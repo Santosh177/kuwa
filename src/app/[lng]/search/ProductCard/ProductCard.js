6bb04059dcf5b5ce44466ef8e3afb40c146511a3
SantoshKumarAgrawal <santosh@feelvaleo.com>
2024-11-18 11:49:24 +0530

    added rating search and related product

--- src/app/[lng]/search/ProductCard/ProductCard.js
+++ src/app/[lng]/search/ProductCard/ProductCard.js
@@ -9,7 +9,7 @@ const ProductCard = ({ cardData={}, searchQuery="" , addToCart = {}, style = {} 
     const router = useRouter();
     console.log("wbejw",cardData)
     // const { productName =0, finalPrice =0, retailPrice = "", currency = "", image = "", id = "", seoUrl = "",dealId="",isDealActive="",isTimerActive="",tag="",tagIconUrl="",productNameArabic="" } = cardData || {}
-    const { productName="",productNameArabic="", finalPrice="" , retailPrice="", currency="", discount="",productImage="",dealId="" ,productId="", variantId="" ,seoUrl="" , tag="",tagIconUrl="",dealInventory="",isDealActive="",isTimerActive="",currentTimerStatus="",normalInventory="",isProductBestSeller = true } = cardData || {}
+    const { productName="",productNameArabic="", finalPrice="" , retailPrice="", currency="", discount="",productImage="",dealId="" ,productId="", variantId="" ,seoUrl="" , tag="",tagIconUrl="",dealInventory="",isDealActive="",isTimerActive="",currentTimerStatus="",normalInventory="",isProductBestSeller = true,ratingsCount,overallRating   } = cardData || {}
     const {listOfLanguages , selectedLanguage, isArabic, isEnglish, changeLanguage={}} = useLanguage();
     // let discount = parseFloat(retailPrice-finalPrice).toFixed(2);
     console.log("isProductBestSeller",isProductBestSeller)
@@ -58,7 +58,16 @@ const ProductCard = ({ cardData={}, searchQuery="" , addToCart = {}, style = {} 
                     </div>
                 </div>
                 <div id="search-container" className={styles.productName}>{isArabic ? productNameArabic : productName}</div>
-                {<div id="search-container" className={styles.discountTag} style={(discount > 0) ? { opacity: 1 } : { opacity: 0 }}><span>Save</span> {currency} {discount}</div>}
+                {ratingsCount > 0 && (
+                <div className={styles.ratingDiv}>
+                  <img
+                  src="https://d25uasl7utydze.cloudfront.net/assets/star-filled.svg"
+                  alt="Star Rating"
+                  />
+                {overallRating} <span>({ratingsCount} {isArabic ? "تقييم" : "ratings"})</span>
+                </div>
+                )}
+                { discount > 0 && <div id="search-container" className={styles.discountTag} style={(discount > 0) ? { opacity: 1 } : { opacity: 0 }}><span>Save</span> {currency} {discount}</div>}
                 {(discount > 0) ? <div className={styles.price}>{currency} {finalPrice}  <span className={[styles.price, styles.retailPrice].join(" ")}>{currency} {retailPrice}</span></div> : <div className={styles.price}>{currency} {finalPrice} </div>}
             </div>
         </div>
